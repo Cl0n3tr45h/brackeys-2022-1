bbb0ed0df06c614db242df9eb6fea717ba8f0f2c
Jamison <jamison.bassett@gmx.de>
2022-02-25 11:15:24 +0100

    cam control + scene save

--- Brackeys2022.1/Assets/CameraController.cs
+++ Brackeys2022.1/Assets/CameraController.cs
@@ -8,6 +8,9 @@ public class CameraController : MonoBehaviour
 
     public float CameraWiggle;
 
+    public float WidthThreshold;
+    public float heightThreshold;
+    public float Speed;
     private Vector3 origin;
     // Start is called before the first frame update
     void Start()
@@ -18,29 +21,30 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var centerX = Player.transform.position.x;
-        var centerY = Player.transform.position.y;
-        var mousePos = Mouse.GetMousePos(0);
-        var deltaX = centerX - mousePos.x;
-        var deltaY = centerY - mousePos.y;
-        deltaX *= CameraWiggle;
-        deltaY *= CameraWiggle;
+        //if (CheckMouseToBounds())
+        //{
+            var centerX = Player.transform.position.x;
+            var centerY = Player.transform.position.y;
+            var mousePos = Mouse.GetMousePos(0);
+            var deltaX = centerX - mousePos.x;
+            var deltaY = centerY - mousePos.y;
+            deltaX *= CameraWiggle;
+            deltaY *= CameraWiggle;
 
-        var finalX =Mathf.Clamp(centerX - deltaX, -10, 10);
-        var finalY = Mathf.Clamp(centerY - deltaY, -8, 8);
-        this.transform.position = origin + new Vector3(finalX, finalY);
-        
+            var finalX = Mathf.Clamp(centerX - deltaX, -10, 10);
+            var finalY = Mathf.Clamp(centerY - deltaY, -8, 8);
+            this.transform.position =new Vector3( Mathf.Lerp(origin.x, origin.x + finalX, Speed), Mathf.Lerp(origin.y, origin.y + finalY, Speed), -10);
+        //}
     }
-/*
+    /*
     private bool CheckMouseToBounds()
     {
         var width = Screen.width;
         var height = Screen.height;
         var mousePos = Mouse.GetMousePos(0);
-        
-        if(mousePos.x > 0 + WidthThreshold && mousePos.x < width - WidthThreshold
-            && )
-        
-        return false;
+
+        return mousePos.x > 0 + WidthThreshold && mousePos.x < width - WidthThreshold
+                                               && mousePos.y > 0 + heightThreshold &&
+                                               mousePos.x < height - heightThreshold;
     }*/
 }
